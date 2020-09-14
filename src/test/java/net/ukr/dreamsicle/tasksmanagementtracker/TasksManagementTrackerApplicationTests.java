8f968de3b712bf90f734353593af7d0347956a10
yurii.loienko <yurii.loienko@sigma.software>
2020-09-14 17:07:03 +0300

    Initial commit

---
+++ src/test/java/net/ukr/dreamsicle/tasksmanagementtracker/TasksManagementTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ukr.dreamsicle.tasksmanagementtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksManagementTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
